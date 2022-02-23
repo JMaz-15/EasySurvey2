48ef5730c9cc3022285ddb98dffd58e978238b5e
JMaz-15 <65188299+JMaz-15@users.noreply.github.com>
2022-02-23 17:36:49 -0500

    Created base buttons for the homepage of our application

--- app/src/main/java/com/example/easysurvey/MainActivity.kt
+++ app/src/main/java/com/example/easysurvey/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.easysurvey
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.easysurvey.ui.theme.EasySurveyTheme
 
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
             EasySurveyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    HomepageButtons()
                 }
             }
         }
@@ -27,14 +31,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomepageButtons() {
+    val text = "You clicked this button"
+    val duration = Toast.LENGTH_LONG
+    val context = LocalContext.current
+    Column{
+        Button(
+            onClick = { 
+                Toast.makeText(context, text, duration).show()
+            }
+        ){ Text(text = "Take a Survey")}
+        Button(
+            onClick = {
+                Toast.makeText(context, text, duration).show()
+            }
+        ){ Text(text = "Create a Survey")}
+        Button(
+            onClick = {
+                Toast.makeText(context, text, duration).show()
+            }
+        ){ Text(text = "See Survey Results")}
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     EasySurveyTheme {
-        Greeting("Android")
     }
 }
